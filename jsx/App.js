3f1b3b60ccad8ddc841ac4821a35a077c3fbce0a
Łukasz <lokers778@gmail.com>
2019-10-31 00:02:37 +0100

    finished configuration webpack, babel, sass

---
+++ jsx/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "../scss/main.scss"
+
+class App extends React.Component{
+render() {
+    return( <div>Hello</div>)
+}
+}
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
